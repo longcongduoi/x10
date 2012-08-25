5ef2819188aca5c4438cfb49409180327af61d1b
Daniel Kang <me@daniel.gs>
2012-08-25 01:02:38 -0700

    Added some fs testing codes. This is very temporary.

--- app1.cpp
+++ app1.cpp
@@ -1,13 +1,114 @@
 #include <iostream>
 #include <x10.h>
 
+using namespace x10;
+
+/*
+ sync open: 10
+ sync close: success
+ fs_test_func()
+ e: 0 f: 10
+ fs_test_functor()
+ e: 0 f: 11
+ lambda
+ e: 0 f: 12
+ e: 0
+ e: 0
+ fs_test_func()
+ e: 0 f: 10
+ fs_test_functor()
+ e: 0 f: 11
+ lambda
+ e: 0 f: 15
+ e: 0
+ e: 0
+ e: 0
+ fs_test_func()
+ e: 0 f: 10
+ fs_test_functor()
+ e: 0 f: 11
+ lambda
+ e: 0 f: 12
+ e: 0
+ e: 0
+ e: 0
+ e: 0
+*/
+
+void fs_test_func(error_t e, file_t f)
+{
+    std::cout << "fs_test_func()" << std::endl;
+    std::cout << "e: " << e << " f: " << f << std::endl;
+    
+    file::close(f, [](error_t e) {
+        std::cout << "e: " << e << std::endl;
+    });
+}
+
+class fs_test_functor
+{
+public:
+    void operator()(error_t e, file_t f)
+    {
+        std::cout << "fs_test_functor()" << std::endl;
+        std::cout << "e: " << e << " f: " << f << std::endl;
+        
+        file::close(f, [](error_t e) {
+            std::cout << "e: " << e << std::endl;
+        });
+    }
+};
+
+void fs_test()
+{
+    auto fd = file::open(readonly, "app1.cpp");
+    std::cout << "sync open: " << fd << std::endl;
+    std::cout << "sync close: " << file::close(fd).str() << std::endl;
+    
+    file::open(readonly, "app1.cpp", fs_test_func);
+    file::open(readonly, "app1.cpp", fs_test_functor());
+    file::open(readonly, "app1.cpp", [](error_t e, file_t f) {
+        std::cout << "lambda" << std::endl;
+        std::cout << "e: " << e << " f: " << f << std::endl;
+        
+        file::open(readonly, "app1.cpp", fs_test_func);
+        file::open(readonly, "app1.cpp", fs_test_functor());
+        file::open(readonly, "app1.cpp", [](error_t e, file_t f) {
+            std::cout << "lambda" << std::endl;
+            std::cout << "e: " << e << " f: " << f << std::endl;
+
+            file::open(readonly, "app1.cpp", fs_test_func);
+            file::open(readonly, "app1.cpp", fs_test_functor());
+            file::open(readonly, "app1.cpp", [](error_t e, file_t f) {
+                std::cout << "lambda" << std::endl;
+                std::cout << "e: " << e << " f: " << f << std::endl;
+                
+                file::close(f, [](error_t e) {
+                    std::cout << "e: " << e << std::endl;
+                });
+            });
+            
+            file::close(f, [](error_t e) {
+                std::cout << "e: " << e << std::endl;
+            });
+        });
+        
+        file::close(f, [](error_t e) {
+            std::cout << "e: " << e << std::endl;
+        });
+    });
+}
+
 int main(int argc, char** argv)
 {
-    auto fd = x10::file::open(x10::readonly, "app1.cpp");
+#if 1
+    fs_test();
+#else
+    auto fd = file::open(readonly, "app1.cpp");
     std::cout << "sync open: " << fd << std::endl;
-    std::cout << "sync close: " << x10::file::close(fd).str() << std::endl;
-
-    x10::file::open("app1.cpp", O_RDONLY, 0, [&](x10::error_t e, x10::file_t f) {
+    std::cout << "sync close: " << file::close(fd).str() << std::endl;
+    
+    file::open("app1.cpp", O_RDONLY, 0, [&](error_t e, file_t f) {
         if(e)
         {
             std::cout << "async open error: " << e.str() << std::endl;
@@ -16,11 +117,10 @@ int main(int argc, char** argv)
 
         std::cout << "async open: " << f << std::endl;
         
-        x10::file::close(f, [](x10::error_t e) {
+        file::close(f, [](error_t e) {
             std::cout << "async close: " << e.str() << std::endl;
         });
-
     });
-
-    return x10::start();
+#endif
+    return start();
 }
